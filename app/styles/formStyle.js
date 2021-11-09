21922547a9ff6a69c3dcf975bca1ae3af10cd08c
Vikas M <vikas200600@gmail.com>
2021-11-09 22:15:39 +0530

    Signup view complete

---
+++ app/styles/formStyle.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    formCard: {
+        backgroundColor: 'white',
+        padding: 20,
+        alignItems: 'center',
+        borderRadius: 10
+    },
+    formHead: {
+        fontSize: 24,
+        letterSpacing: 1,
+        fontWeight: 'bold',
+        paddingBottom: 20
+    }
+})
+
+export default styles;
---
+++ app/views/signup.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { SafeAreaView, Text, TextInput, View, Pressable } from 'react-native';
+import { Formik } from 'formik';
+
+import Global from './../styles/global';
+import Styles from './../styles/styles';
+import FormStyle from './../styles/formStyle';
+
+function SignUp(props) {
+    return (
+        <SafeAreaView style={Global.container}>
+            <View style={FormStyle.formCard}>
+                <Text style={FormStyle.formHead}>SIGNUP</Text>
+                <Formik
+                    initialValues={{ name: '', password: '', email: '', phone: '', profession: '' }}
+                    onSubmit={(values, actions) => {
+                        actions.resetForm();
+                    }}
+                >
+                    {props => (
+                        <View>
+                            <TextInput
+                                style={Global.input}
+                                placeholder="Enter Name"
+                                onChange={props.handleChange('name')}
+                                onBlur={props.handleBlur('name')}
+                                value={props.values.name}
+                            />
+                            <TextInput
+                                style={Global.input}
+                                placeholder="Enter Password"
+                                onChange={props.handleChange('password')}
+                                onBlur={props.handleBlur('password')}
+                                value={props.values.password}
+                            />
+                            <TextInput
+                                style={Global.input}
+                                placeholder="Enter Email"
+                                onChange={props.handleChange('email')}
+                                onBlur={props.handleBlur('email')}
+                                value={props.values.email}
+                            />
+                            <TextInput
+                                style={Global.input}
+                                placeholder="Enter Phone"
+                                onChange={props.handleChange('phone')}
+                                onBlur={props.handleBlur('phone')}
+                                value={props.values.phone}
+                            />
+                            <TextInput
+                                style={Global.input}
+                                placeholder="Enter Profession"
+                                onChange={props.handleChange('profession')}
+                                onBlur={props.handleBlur('profession')}
+                                value={props.values.profession}
+                            />
+                            <Pressable
+                                onPress={props.handleSubmit}
+                                style={Styles.signBut}>
+                                <Text style={Styles.butText}>SIGNUP</Text>
+                            </Pressable>
+                        </View>
+                    )}
+                </Formik>
+            </View>
+        </SafeAreaView >
+    );
+}
+
+export default SignUp;
